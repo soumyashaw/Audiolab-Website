986b148a933922cec5d9b59d1ca578524be2b479
Soumya Shaw <37841510+soumyashaw@users.noreply.github.com>
2024-04-02 12:44:06 +0200

    skeleton complete

--- js/script.js
+++ js/script.js
@@ -100,7 +100,7 @@ buttonAlgo4.addEventListener('click', function() {
 
 var noisyAudio = document.getElementById("noisyAudio");
 var echoAudio = document.getElementById("echoAudio");
-var playIcon1 = document.getElementById("play-icon-3");
+var playIcon3 = document.getElementById("play-icon-3");
 var selectedAudioAction1;
 
 document.getElementById('action1-1').addEventListener('click', function() {
@@ -112,7 +112,7 @@ document.getElementById('action1-1').addEventListener('click', function() {
     var arrow1 = document.getElementById('test-arrow-1')
     var arrowHead1 = document.getElementById('test-arrow-head-1')
     content2.style.display = 'none';
-    selectedAudioAction1 = noisyAudio;
+    selectedAudioAction1 = 'noisyAudio';
     content1.style.display = 'block';
     oops1.style.opacity = 0;
     metrics1.style.opacity = 0;
@@ -138,7 +138,7 @@ document.getElementById('action1-2').addEventListener('click', function() {
     var arrow1 = document.getElementById('test-arrow-1')
     var arrowHead1 = document.getElementById('test-arrow-head-1')
     content2.style.display = 'none';
-    selectedAudioAction1 = echoAudio;
+    selectedAudioAction1 = 'echoAudio';
     content1.style.display = 'block';
     oops1.style.opacity = 0;
     metrics1.style.opacity = 0;
@@ -219,48 +219,295 @@ document.getElementById('test-it').addEventListener('click', function() {
 });
 
 noisyAudio.addEventListener("ended", function() {
-    playIcon1.innerHTML = "&#9658;";
+    playIcon3.innerHTML = "&#9658;";
 });
 
 echoAudio.addEventListener("ended", function() {
-    playIcon2.innerHTML = "&#9658;";
+    playIcon3.innerHTML = "&#9658;";
 });
 
 document.getElementById('sample1').addEventListener('click', function() {
-    if (selectedAudioAction1.paused) {
-        selectedAudioAction1.play();
-        document.getElementById("play-icon-3").innerHTML = "&#9616;&#9616;";
-    } else {
-        selectedAudioAction1.pause();
-        selectedAudioAction1.currentTime = 0;
-        document.getElementById("play-icon-3").innerHTML = "&#9658;";
+    if (selectedAudioAction1 == 'noisyAudio') {
+        if (noisyAudio.paused) {
+            noisyAudio.play();
+            document.getElementById("play-icon-3").innerHTML = "&#9616;&#9616;";
+        } else {
+            noisyAudio.pause();
+            noisyAudio.currentTime = 0;
+            document.getElementById("play-icon-3").innerHTML = "&#9658;";
+        }
+    }
+    else {
+        if (echoAudio.paused) {
+            echoAudio.play();
+            document.getElementById("play-icon-3").innerHTML = "&#9616;&#9616;";
+        } else {
+            echoAudio.pause();
+            echoAudio.currentTime = 0;
+            document.getElementById("play-icon-3").innerHTML = "&#9658;";
+        }
     }
 });
 
 
+// Real World Effects 2
 
+var lowBitRateAudio = document.getElementById("lowBitRateAudio");
+var gaussianNoiseAudio = document.getElementById("gaussianNoiseAudio");
+var playIcon4 = document.getElementById("play-icon-4");
+var selectedAudioAction2;
 
-
-
-
-document.getElementById('compute-btn').addEventListener('click', function() {
-    showComputingAnimation();
+document.getElementById('action2-1').addEventListener('click', function() {
+    var content1 = document.getElementById('addition-animation-2');
+    var content2 = document.getElementById('effects-results-2');
+    var content3 = document.getElementById('test-outer-2');
+    var metrics2 = document.getElementById('metrics-2');
+    var oops2 = document.getElementById('oops-2');
+    var arrow2 = document.getElementById('test-arrow-2')
+    var arrowHead2 = document.getElementById('test-arrow-head-2')
+    content2.style.display = 'none';
+    selectedAudioAction2 = 'lowBitRateAudio';
+    content1.style.display = 'block';
+    oops2.style.opacity = 0;
+    metrics2.style.opacity = 0;
+    arrow2.style.backgroundColor = 'rgb(0, 122, 253)';
+    arrowHead2.style.borderBottom = '100px solid rgb(0, 122, 253)';
+    
     setTimeout(function() {
-        showOutput(true); // Simulating computation result
-    }, 2000); // Simulating computation delay
+        content2.style.display = 'flex';
+        content3.style.opacity = 0;
+    }, 2500);
+
+    setTimeout(function() {
+        content3.style.opacity = 1;
+    }, 4500);
+});
+
+document.getElementById('action2-2').addEventListener('click', function() {
+    var content1 = document.getElementById('addition-animation-2');
+    var content2 = document.getElementById('effects-results-2');
+    var content3 = document.getElementById('test-outer-2');
+    var metrics2 = document.getElementById('metrics-2');
+    var oops2 = document.getElementById('oops-2');
+    var arrow2 = document.getElementById('test-arrow-2')
+    var arrowHead2 = document.getElementById('test-arrow-head-2')
+    content2.style.display = 'none';
+    selectedAudioAction2 = 'gaussianNoiseAudio';
+    content1.style.display = 'block';
+    oops2.style.opacity = 0;
+    metrics2.style.opacity = 0;
+    arrow2.style.backgroundColor = 'rgb(0, 122, 253)';
+    arrowHead2.style.borderBottom = '100px solid rgb(0, 122, 253)';
+    
+    setTimeout(function() {
+        content2.style.display = 'flex';
+        content3.style.opacity = 0;
+    }, 2500);
+
+    setTimeout(function() {
+        content3.style.opacity = 1;
+    }, 4500);
+});
+
+document.getElementById('test-arrow-2').addEventListener('click', function() {
+    var metrics2 = document.getElementById('metrics-2');
+    var computing2 = document.getElementById('computing-2')
+    var oops2 = document.getElementById('oops-2');
+    var arrow2 = document.getElementById('test-arrow-2')
+    var arrowHead2 = document.getElementById('test-arrow-head-2')
+    computing2.style.display = 'block';
+    metrics2.style.opacity = 0;
+    oops2.style.opacity = 0;
+    arrow2.style.backgroundColor = 'rgb(6, 215, 108)';
+    arrowHead2.style.borderBottom = '100px solid rgb(6, 215, 108)';
+
+    setTimeout(function() {
+        computing2.style.display = 'none';
+    }, 5000);
+    setTimeout(function() {
+        metrics2.style.opacity = 1;
+        oops2.style.opacity = 1;
+    }, 5300);
+});
+
+document.getElementById('test-arrow-head-2').addEventListener('click', function() {
+    var metrics2 = document.getElementById('metrics-2');
+    var computing2 = document.getElementById('computing-2')
+    var oops2 = document.getElementById('oops-2');
+    var arrow2 = document.getElementById('test-arrow-2')
+    var arrowHead2 = document.getElementById('test-arrow-head-2')
+    computing2.style.display = 'block';
+    metrics2.style.opacity = 0;
+    oops2.style.opacity = 0;
+    arrow2.style.backgroundColor = 'rgb(6, 215, 108)';
+    arrowHead2.style.borderBottom = '100px solid rgb(6, 215, 108)';
+
+    setTimeout(function() {
+        computing2.style.display = 'none';
+    }, 5000);
+    setTimeout(function() {
+        metrics2.style.opacity = 1;
+        oops2.style.opacity = 1;
+    }, 5300);
+});
+
+document.getElementById('test-it-2').addEventListener('click', function() {
+    var metrics2 = document.getElementById('metrics-2');
+    var computing2 = document.getElementById('computing-2')
+    var oops2 = document.getElementById('oops-2');
+    var arrow2 = document.getElementById('test-arrow-2')
+    var arrowHead2 = document.getElementById('test-arrow-head-2')
+    computing2.style.display = 'block';
+    metrics2.style.opacity = 0;
+    oops2.style.opacity = 0;
+    arrow2.style.backgroundColor = 'rgb(6, 215, 108)';
+    arrowHead2.style.borderBottom = '100px solid rgb(6, 215, 108)';
+
+    setTimeout(function() {
+        computing2.style.display = 'none';
+    }, 5000);
+    setTimeout(function() {
+        metrics2.style.opacity = 1;
+        oops2.style.opacity = 1;
+    }, 5300);
+});
+
+lowBitRateAudio.addEventListener("ended", function() {
+    playIcon5.innerHTML = "&#9658;";
+});
+
+gaussianNoiseAudio.addEventListener("ended", function() {
+    playIcon6.innerHTML = "&#9658;";
+});
+
+document.getElementById('sample2').addEventListener('click', function() {
+    if (selectedAudioAction2 == 'lowBitRateAudio') {
+        if (lowBitRateAudio.paused) {
+            lowBitRateAudio.play();
+            document.getElementById("play-icon-4").innerHTML = "&#9616;&#9616;";
+        } else {
+            lowBitRateAudio.pause();
+            lowBitRateAudio.currentTime = 0;
+            document.getElementById("play-icon-4").innerHTML = "&#9658;";
+        }
+    }
+    else {
+        if (gaussianNoiseAudio.paused) {
+            gaussianNoiseAudio.play();
+            document.getElementById("play-icon-4").innerHTML = "&#9616;&#9616;";
+        } else {
+            gaussianNoiseAudio.pause();
+            gaussianNoiseAudio.currentTime = 0;
+            document.getElementById("play-icon-4").innerHTML = "&#9658;";
+        }
+    }
 });
 
 
-function showComputingAnimation() {
-    document.getElementById('computing-animation').style.display = 'block';
-}
+// Real World Effects 3
 
-function showOutput(isRealSpeaker) {
-    document.getElementById('computing-animation').style.display = 'none';
-    var outputDiv = document.getElementById('output');
-    if (isRealSpeaker) {
-        outputDiv.innerHTML = "Output matches real speaker.";
+var volReducedAudio = document.getElementById("volReducedAudio");
+var playIcon4 = document.getElementById("play-icon-4");
+
+document.getElementById('action3-1').addEventListener('click', function() {
+    var content1 = document.getElementById('addition-animation-3');
+    var content2 = document.getElementById('effects-results-3');
+    var content3 = document.getElementById('test-outer-3');
+    var metrics3 = document.getElementById('metrics-3');
+    var oops3 = document.getElementById('oops-3');
+    var arrow3 = document.getElementById('test-arrow-3')
+    var arrowHead3 = document.getElementById('test-arrow-head-3')
+    content2.style.display = 'none';
+    content1.style.display = 'block';
+    oops3.style.opacity = 0;
+    metrics3.style.opacity = 0;
+    arrow3.style.backgroundColor = 'rgb(0, 122, 253)';
+    arrowHead3.style.borderBottom = '100px solid rgb(0, 122, 253)';
+    
+    setTimeout(function() {
+        content2.style.display = 'flex';
+        content3.style.opacity = 0;
+    }, 2500);
+
+    setTimeout(function() {
+        content3.style.opacity = 1;
+    }, 4500);
+});
+
+document.getElementById('test-arrow-3').addEventListener('click', function() {
+    var metrics3 = document.getElementById('metrics-3');
+    var computing3 = document.getElementById('computing-3')
+    var oops3 = document.getElementById('oops-3');
+    var arrow3 = document.getElementById('test-arrow-3')
+    var arrowHead3 = document.getElementById('test-arrow-head-3')
+    computing3.style.display = 'block';
+    metrics3.style.opacity = 0;
+    oops3.style.opacity = 0;
+    arrow3.style.backgroundColor = 'rgb(6, 215, 108)';
+    arrowHead3.style.borderBottom = '100px solid rgb(6, 215, 108)';
+
+    setTimeout(function() {
+        computing3.style.display = 'none';
+    }, 5000);
+    setTimeout(function() {
+        metrics3.style.opacity = 1;
+        oops3.style.opacity = 1;
+    }, 5300);
+});
+
+document.getElementById('test-arrow-head-3').addEventListener('click', function() {
+    var metrics3 = document.getElementById('metrics-3');
+    var computing3 = document.getElementById('computing-3')
+    var oops3 = document.getElementById('oops-3');
+    var arrow3 = document.getElementById('test-arrow-3')
+    var arrowHead3 = document.getElementById('test-arrow-head-3')
+    computing3.style.display = 'block';
+    metrics3.style.opacity = 0;
+    oops3.style.opacity = 0;
+    arrow3.style.backgroundColor = 'rgb(6, 215, 108)';
+    arrowHead3.style.borderBottom = '100px solid rgb(6, 215, 108)';
+
+    setTimeout(function() {
+        computing3.style.display = 'none';
+    }, 5000);
+    setTimeout(function() {
+        metrics3.style.opacity = 1;
+        oops3.style.opacity = 1;
+    }, 5300);
+});
+
+document.getElementById('test-it-3').addEventListener('click', function() {
+    var metrics3 = document.getElementById('metrics-3');
+    var computing3 = document.getElementById('computing-3')
+    var oops3 = document.getElementById('oops-3');
+    var arrow3 = document.getElementById('test-arrow-3')
+    var arrowHead3 = document.getElementById('test-arrow-head-3')
+    computing3.style.display = 'block';
+    metrics3.style.opacity = 0;
+    oops3.style.opacity = 0;
+    arrow3.style.backgroundColor = 'rgb(6, 215, 108)';
+    arrowHead3.style.borderBottom = '100px solid rgb(6, 215, 108)';
+
+    setTimeout(function() {
+        computing3.style.display = 'none';
+    }, 5000);
+    setTimeout(function() {
+        metrics3.style.opacity = 1;
+        oops3.style.opacity = 1;
+    }, 5300);
+});
+
+volReducedAudio.addEventListener("ended", function() {
+    playIcon4.innerHTML = "&#9658;";
+});
+
+document.getElementById('sample3').addEventListener('click', function() {
+    if (volReducedAudio.paused) {
+        volReducedAudio.play();
+        document.getElementById("play-icon-4").innerHTML = "&#9616;&#9616;";
     } else {
-        outputDiv.innerHTML = "Output does not match real speaker.";
+        volReducedAudio.pause();
+        volReducedAudio.currentTime = 0;
+        document.getElementById("play-icon-4").innerHTML = "&#9658;";
     }
-}
+});
